3aef8f4e0b4fa8b63bf239f4d8260a9d0ba6852c
Elizabeth Paige Harper <elizabeth.harper@foxcapades.io>
2025-06-20 13:40:59 -0400

    fix missing "+ 1", fix issue with unintentional cast to long in value comparisons

--- src/commonTest/kotlin/io/klibs/math/big/BigIntTest.kt
+++ src/commonTest/kotlin/io/klibs/math/big/BigIntTest.kt
@@ -4,8 +4,15 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class BigIntTest {
+  // ArrayIndexOutOfBoundsException calling BigInt.and(BigInt)
   @Test
-  fun issue_2_shr_to_zero_causes_array_out_of_bounds() {
+  fun issue_1() {
+    assertEquals(BigInt.One, BigInt.One and BigInt.One)
+  }
+
+  // ArrayIndexOutOfBoundsException calling toString() on 0 value obtained via shr
+  @Test
+  fun issue_2() {
     assertEquals("0", bigIntOf(1).shr(1).toString())
     assertEquals("0", bigIntOf(1).shl(-7).toString())
   }
